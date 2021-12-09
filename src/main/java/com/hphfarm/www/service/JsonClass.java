62953734a9e8cda1880028cf2928fdfc1a085001
koreavc1 <koreavc1@HOME-PC>
2021-12-09 15:21:34 +0900

    ex

--- src/main/java/com/hphfarm/www/service/JsonClass.java
+++
@@ -1,7 +0,0 @@
-package com.hphfarm.www.service;
-
-public class JsonClass {
-
-	String[] arr = {"11B10101","11B10101","11B10103","11D10302","11C10303","11C20502","11C20401","11C20404","11F20501"};
-
-}
--- src/main/java/com/hphfarm/www/service/WeatherService.java
+++
@@ -1,6 +0,0 @@
-package com.hphfarm.www.service;
-
-public interface WeatherService {
-	// 날씨 공공데이터 가져오기
-	String selectData(int pageNo) throws Exception;
-}
